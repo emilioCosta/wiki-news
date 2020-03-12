8d4a5acdf247198cce72bbf50efb101318c66bf0
Emilio <emilio.costa@dextra-sw.com>
2020-03-12 20:47:12 -0300

    Updates

---
+++ precache-manifest.76d23adcf8dd46ad5588accc52e99ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a610f487832927954b9b0f73805d5f4",
+    "url": "/wiki-news/index.html"
+  },
+  {
+    "revision": "fb889e875579df93a2a7",
+    "url": "/wiki-news/static/css/main.81824659.chunk.css"
+  },
+  {
+    "revision": "e0c4fd7bf84ef37852e6",
+    "url": "/wiki-news/static/js/2.f9d52916.chunk.js"
+  },
+  {
+    "revision": "552fc37e66e521b8ef1b4259470f149f",
+    "url": "/wiki-news/static/js/2.f9d52916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb889e875579df93a2a7",
+    "url": "/wiki-news/static/js/main.56899a98.chunk.js"
+  },
+  {
+    "revision": "bbf348afea60e8e828e2",
+    "url": "/wiki-news/static/js/runtime-main.fb8b4950.js"
+  }
+]);
